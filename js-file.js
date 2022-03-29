601820f1edaf6c72d7c1ef2f9713b8857ada231b
Derek Nunes <dereknunes28@gmail.com>
2022-03-29 00:40:07 -0500

    add button, make modal open and close properly

--- js-file.js
+++ js-file.js
@@ -21,6 +21,7 @@ function addBookToLibrary(book) {
         const card = document.createElement('div');
         card.classList.add('card');
         card.innerHTML = `
+            <button class="delete-entry">x</button>
             <div class="title">${book.title}</div>
             <div class="author">${book.author}</div>
             <div class="pages">${book.pages}</div>
@@ -30,6 +31,48 @@ function addBookToLibrary(book) {
         bookCards.appendChild(card);        
     }
 }
+
+// Add book button to open hidden modal
+document.querySelector("#addBook").addEventListener('click', (e) => {
+    showModal();
+});
+
+function showModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add("modal-open");
+    const grid = document.querySelector(".grid");
+    grid.classList.add("modal-open");
+}
+
+// Close modal pressing 'x' in modal block
+document.querySelector('.close-modal').addEventListener('click', (e) => {
+    closeModal();
+});
+
+// Close modal when you click outside of modal **need to make it work
+// document.querySelector('.modal').addEventListener('click', function(e) {
+//     const isOutside = e.target.closest('.modal-content');
+//     if (isOutside) {
+//         closeModal();
+//     }
+// })
+
+function closeModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.remove("modal-open");
+    const grid = document.querySelector(".grid");
+    grid.classList.remove("modal-open");
+    document.getElementById('form').reset();
+}
+
+// submit data and add card ***need to make it work
+// document.querySelector('#modal-submit').addEventListener('click', function(e) {
+//     const title = document.querySelector('#title').textContent;
+//     const author = document.querySelector('#author').textContent;
+//     const pages = document.querySelector('#pages').textContent;
+//     const newBook = Book(title, author, pages, no);
+//     addBookToLibrary(newBook);
+// })
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'yes');
 const Hobbit = new Book('Hobbit', 'J.R.R. Tolkien', 295, 'no');
